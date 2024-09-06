16882cb782a2890e4b5b2262a15814b37c07f08d
Aqua Fir <aquafirs@gmail.com>
2024-09-06 14:04:57 -0500

    Add Toggle

---
+++ ACE.Shared/Helpers/ToggleExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace ACE.Shared.Helpers;
+
+public static class ToggleExtensions
+{
+    //Todo: maybe event handlers?
+
+    public static bool Toggle(this WorldObject wo, PropertyBool prop)
+    {
+        var value = !(wo.GetProperty(prop) ?? false);
+        wo.SetProperty(prop, value);
+
+        return value;
+    }
+    public static bool Toggle(this WorldObject wo, FakeBool prop)
+    {
+        var value = !(wo.GetProperty(prop) ?? false);
+        wo.SetProperty(prop, value);
+
+        return value;
+    }
+}
